77d911a5589b9ca06234ee6f14a430d31725c1c9
DariusClover <dario503030@gmail.com>
2026-07-19 02:31:01 -0500

    Actualizar nombres de aplicaciones en la configuración de Django para incluir el prefijo 'app.'

--- backend/app/egresados/apps.py
+++ backend/app/egresados/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EgresadosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'egresados'
+    name = 'app.egresados'
--- backend/app/supletorios/apps.py
+++ backend/app/supletorios/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SupletoriosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'supletorios'
+    name = 'app.supletorios'
